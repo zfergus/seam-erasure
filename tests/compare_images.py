7588acdfd8235e1b07c0497df3aecbe40be8a8b0
zfergus <zfergus@nyu.edu>
2019-10-10 12:17:12 -0400

    Fixed travis diff test input

--- tests/compare_images.py
+++ tests/compare_images.py
@@ -9,8 +9,8 @@ from SeamErasure.texture import load_texture
 
 assert(len(sys.argv) == 3)
 
-image_a = numpy.array(load_texture(sys.argv[1]), dtype=float)
-image_b = numpy.array(load_texture(sys.argv[2]), dtype=float)
+image_a = numpy.array(load_texture(sys.argv[1]), dtype=float) / 255.
+image_b = numpy.array(load_texture(sys.argv[2]), dtype=float) / 255.
 
 assert(image_a.shape == image_b.shape)
 assert(numpy.linalg.norm(image_a - image_b) < 1 / 255.)
